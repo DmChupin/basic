d3ee27a5be650067d55437f9133f480e4e88c950
unknown <mr.dmitriych@gmail.com>
2023-07-02 21:25:10 +0300

    feat: reviews in progress

--- src/app/components/pages/reviews/reviews.component.ts
+++ src/app/components/pages/reviews/reviews.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-reviews',
 	templateUrl: './reviews.component.html',
 	styleUrls: ['./reviews.component.less'],
 })
-export class ReviewsComponent {}
+export class ReviewsComponent implements OnInit {
+	@Input()
+	hotelId!: number;
+
+	constructor(private readonly service: ReviewsService);
+
+	reviews: IReview[] = [];
+
+	ngOnInit(): void {
+		this.getReviews();
+	}
+
+	getReviews() {
+		this.service.getReviews(this.hotelId).subscribe(reviews => (this.reviews = reviews));
+	}
+}
--- src/app/components/pages/reviews/reviews.module.ts
+++ src/app/components/pages/reviews/reviews.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReviewsComponent } from './reviews.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
 	declarations: [ReviewsComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, MatListModule],
+	exports: [ReviewsComponent],
 })
 export class ReviewsModule {}
